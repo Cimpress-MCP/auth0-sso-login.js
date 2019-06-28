455ed282089ebbe726d8db361c3bba9a1bd1ea57
Daan Boerlage <dboerlage@cimpress.com>
2019-06-28 15:28:25 +0200

    Upgrade dependencies to fix security vulnerabilities.

--- test/.setup.js
+++ test/.setup.js
@@ -2,7 +2,7 @@ const jsdom = require('jsdom');
 
 const exposedProperties = ['window', 'navigator', 'document'];
 
-let dom = new jsdom.JSDOM('<!doctype html><html><body></body></html>');
+let dom = new jsdom.JSDOM('<!doctype html><html><body></body></html>', { url: 'https://unit-test.com/'});
 global.document = dom.window.document;
 global.window = dom.window;
 global.window.encodeURIComponent = function() {};
